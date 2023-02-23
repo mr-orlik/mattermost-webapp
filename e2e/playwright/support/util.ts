cebc58e47d92b651908070b8074d3ee94495ab7d
Saturnino Abril <saturnino.abril@gmail.com>
2023-02-23 16:20:47 +0800

    add test for boards

--- e2e/playwright/support/util.ts
+++ e2e/playwright/support/util.ts
@@ -4,6 +4,10 @@
 import {v4 as uuidv4} from 'uuid';
 import {ViewportSize} from '@playwright/test';
 
+import {isSmallScreen, shouldSkipInSmallScreen, shouldRunInLinux, waitForAnimationEnd} from './test_helper';
+
+export {isSmallScreen, shouldSkipInSmallScreen, shouldRunInLinux, waitForAnimationEnd};
+
 const second = 1000;
 const minute = 60 * 1000;
 
